d19af96a87babe4eb42d4ca413f0a80e21486cb0
ersalomo <ersalomo2002@gmail.com>
2023-05-14 11:52:07 +0700

    fixed error token

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,9 +1,10 @@
 import jwt from 'jsonwebtoken';
-import { logger } from './loggers';
 import CONFIG from '../config/environment';
+import AccessTokenError from '../responses/ApiError/AccessTokenError ';
 
 export const signJWT = async (payload: Object, options?: jwt.SignOptions | undefined) => {
-  const privateKey = await CONFIG.jwt_private_key() || CONFIG.PRIVATE_KEY
+  const privateKey = CONFIG.PRIVATE_KEY || await CONFIG.jwt_private_key()
+  if (!privateKey) throw new AccessTokenError('Missing private key')
   return jwt.sign(payload, privateKey, {
     ...(options && options),
     algorithm: 'RS256',
@@ -11,20 +12,11 @@ export const signJWT = async (payload: Object, options?: jwt.SignOptions | undef
 }
 
 export const verifyJWT = async (token: string) => {
-  const publicKey = await CONFIG.jwt_public_key() || CONFIG.PUBLIC_KEY
-  try {
-    const decoded = jwt.verify(token, publicKey);
-    return {
-      valid: true,
-      expired: false,
-      decoded
-    }
-  } catch (error: any) {
-    logger.error(`Err :[jwt] ${error}`);
-    return {
-      valid: false,
-      expired: error.message === 'jwt is expired or not eligible to use',
-      decoded: null,
-    }
+  const publicKey = CONFIG.PUBLIC_KEY || await CONFIG.jwt_public_key()
+  const decoded = jwt.verify(token, publicKey);
+  return {
+    valid: true,
+    expired: false,
+    decoded
   }
 }
