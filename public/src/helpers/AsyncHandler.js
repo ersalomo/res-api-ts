c3b8524b3394cd183a3b8fac89a3ea725b7e2bc4
ersalomo <ersalomo2002@gmail.com>
2023-05-14 13:01:20 +0700

    edit gitignore

--- public/src/helpers/AsyncHandler.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (exec) => (req, res, next) => {
-    exec(req, res, next).catch(next);
-};
-//# sourceMappingURL=AsyncHandler.js.map
